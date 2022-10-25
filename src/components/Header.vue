dcbeb790e03650611331f463e713540bed42ff7e
faruuhan <farhnn.r@gmail.com>
2022-10-25 21:08:21 +0700

    change content header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-slate-800 text-white py-6">
-    <div class="container mx-auto">
-      <h3 class="font-semibold">vue-movies</h3>
+    <div class="container mx-auto px-4 lg:px-0">
+      <router-link :to="{ path: '/' }">
+        <h3 class="font-semibold hover:text-slate-600">vue-movies</h3>
+      </router-link>
     </div>
   </div>
 </template>
