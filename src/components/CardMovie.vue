74885cea83d16dd2fe587798db98ce62c052af87
faruuhan <farhnn.r@gmail.com>
2022-10-26 22:38:57 +0700

    edited card movie

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -3,16 +3,18 @@
     <img
       :src="pathImg + movie.poster_path"
       :alt="movie.poster_path"
-      class="rounded mb-2"
+      class="rounded mb-2 w-full"
     />
-    <router-link :to="{ path: '/detail/' + movie.id }">
-      <h3 class="font-semibold hover:text-blue-500">
-        {{ movie.original_title }}
-      </h3>
-    </router-link>
-    <p class="text-slate-400">
-      {{ dayjs(movie.release_date).format("MMM DD, YYYY") }}
-    </p>
+    <div class="px-2 py-4">
+      <router-link :to="{ path: '/detail/' + movie.id }">
+        <h3 class="font-semibold hover:text-blue-500">
+          {{ movie.original_title }}
+        </h3>
+      </router-link>
+      <p class="text-slate-400">
+        {{ dayjs(movie.release_date).format("MMM DD, YYYY") }}
+      </p>
+    </div>
   </div>
 </template>
 
